82ce08f8512be58453a77250d11fcfe1aa65ef04
Tiffany Chen <tiffanysta.c@gmail.com>
2017-01-05 19:56:24 -0500

    added dictionaryToArray function

--- SpellCheck.java
+++ SpellCheck.java
@@ -2,54 +2,62 @@ public class SpellCheck{
 
 
      /* public int commonSubstring(String A, String B) {
-	String current = "";
-	for (int i = 0; i < A.length(); i++) {
-	    for (int j = B.length(); j > 0; j++) {
+    String current = "";
+    for (int i = 0; i < A.length(); i++) {
+        for (int j = B.length(); j > 0; j++) {
     */
 
+    private static final ArrayList alphabetical, reversed;
+    
+    public static ArrayList dictionaryToArray(){
+        ArrayList alphabetical = new ArrayList();
+        try {
+            Scanner sc = new Scanner(new File("words.txt"));
+            sc.useDelimiter("\n");
+            while (sc.hasNext("\n")){
+            alphabetical.add(sc.next());
+            }
+            return alphabetical;
+        }
+        catch (FileNotFoundException e){
+            return alphabetical;
+        }
+    }
 
     public static int charMatches(String A, String B) {
-	int matchCount = 0;
-	String first, second;
-	if (A.length() <= B.length()) {//this is to prevent index out of bounds error
-	    first = A;
-	    second = B;
-	}
-	else {
-	    first = B;
-	    second = A;
-	}
-	for (int i = 1; i < first.length()-1; i++) {//runs through shorter word and checks whether the other word has a matching character within one position
-	    if (first.charAt(i) == second.charAt(i) ||
-		first.charAt(i) == second.charAt(i-1) ||
-		first.charAt(i) == second.charAt(i+1)) {
-		//System.out.println(first.charAt(i));
-		//System.out.println(second.charAt(i));
-		matchCount += 1;
-	    }
-	}
-	return matchCount;
+    int matchCount = 0;
+    String first, second;
+    if (A.length() <= B.length()) {//this is to prevent index out of bounds error
+        first = A;
+        second = B;
+    }
+    else {
+        first = B;
+        second = A;
+    }
+    for (int i = 1; i < first.length()-1; i++) {//runs through shorter word and checks whether the other word has a matching character within one position
+        if (first.charAt(i) == second.charAt(i) ||
+        first.charAt(i) == second.charAt(i-1) ||
+        first.charAt(i) == second.charAt(i+1)) {
+        //System.out.println(first.charAt(i));
+        //System.out.println(second.charAt(i));
+        matchCount += 1;
+        }
+    }
+    return matchCount;
     }
 
     public static double matchRatio(String A, String B) {
-	return (double)charMatches(A, B)/A.length();
+        return (double)charMatches(A, B)/A.length();
     }
 
     
     public static void main(String[] args){
-<<<<<<< HEAD
         Window w = new Window();
         w.setVisible(true);
         System.out.println(charMatches("quadratic","chicken"));
-	System.out.println(charMatches("pisza", "pizza"));
-	System.out.println(charMatches("pissza", "pizza"));
-=======
-		Window w = new Window();
-		w.setVisible(true);
-        System.out.println(matchRatio("quadratic","chicken"));
-	System.out.println(matchRatio("pisza", "pizza"));
-	System.out.println(matchRatio("pissza", "pizza"));
->>>>>>> eb38fe2368fcfbc70b8295cfbc2157a06b320916
+        System.out.println(charMatches("pisza", "pizza"));
+        System.out.println(charMatches("pissza", "pizza"));
     }
 
     
