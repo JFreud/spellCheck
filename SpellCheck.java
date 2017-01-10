7d09b82505f5585c2d921f21957a5e39aed2f322
Tiffany Chen <tiffanysta.c@gmail.com>
2017-01-09 20:24:04 -0500

    made minor changes

--- SpellCheck.java
+++ SpellCheck.java
@@ -31,60 +31,60 @@ public class SpellCheck{
         for (int i = 0; i < alphabetical.size(); i++){
             reversed.add(i, new StringBuilder(alphabetical.get(i)).reverse().toString());
         }
-	Collections.sort(reversed);
-	
+    Collections.sort(reversed);
+    
     }
 
     public static void inputtedToArray(String inputted){ // this function takes in the inputted text and turns it into an ArrayList
         Scanner sc = new Scanner(inputted);
-    	input = new ArrayList<String>();
-    	sc.useDelimiter(" ");
-    	while (sc.hasNext()){
-    	    input.add(sc.next().toLowerCase());
-    	}
+        input = new ArrayList<String>();
+        sc.useDelimiter(" ");
+        while (sc.hasNext()){
+            input.add(sc.next().toLowerCase());
+        }
     }
 
     public static void commoners(){ // this function takes in the inputted text and turns it into an ArrayList
         File fileName = new File("common.txt");
-	Scanner sc = new Scanner(fileName);
-    	common = new ArrayList<String>();
-    	sc.useDelimiter(" ");
-    	while (sc.hasNext()){
-    	    common.add(sc.next().toLowerCase());
-    	}
+        Scanner sc = new Scanner(fileName);
+        common = new ArrayList<String>();
+        sc.useDelimiter(" ");
+        while (sc.hasNext()){
+            common.add(sc.next().toLowerCase());
+        }
     }
 
     public static int charMatches(String A, String B){
-	int matchCount = 0;
-	if (A.length() == B.length()){
-	    for (int i = 0; i < A.length(); i ++){
-		if (A.charAt(i) == B.charAt(i)){
-		    matchCount ++;
-		}
-	    }
-	}
-	else {
-	    String first, second;
-	    if (A.length() <= B.length()) {//this is to prevent index out of bounds error
-		first = A;
-		second = B;
-	    }
-	    else {
-		first = B;
-		second = A;
-	    }
-	    for (int i = 1; i < first.length()-1; i++) {//runs through shorter word and checks whether the other word has a matching character within one position
-		if (first.charAt(i) == second.charAt(i) ||
-		    first.charAt(i) == second.charAt(i-1) ||
-		    first.charAt(i) == second.charAt(i+1))
-		    {
-			//System.out.println(first.charAt(i));
-			//System.out.println(second.charAt(i));
-			matchCount += 1;
-		    }
-	    }
-	}
-	return matchCount;
+        int matchCount = 0;
+        if (A.length() == B.length()){
+            for (int i = 0; i < A.length(); i ++){
+                if (A.charAt(i) == B.charAt(i)){
+                    matchCount ++;
+                }
+            }
+        }
+        else {
+            String first, second;
+            if (A.length() <= B.length()) {//this is to prevent index out of bounds error
+                first = A;
+                second = B;
+            }
+            else {
+                first = B;
+                second = A;
+            }
+            for (int i = 1; i < first.length()-1; i++) {//runs through shorter word and checks whether the other word has a matching character within one position
+                if (first.charAt(i) == second.charAt(i) ||
+                    first.charAt(i) == second.charAt(i-1) ||
+                    first.charAt(i) == second.charAt(i+1))
+                    {
+                    //System.out.println(first.charAt(i));
+                    //System.out.println(second.charAt(i));
+                    matchCount += 1;
+                    }
+            }
+        }
+        return matchCount;
     }
 
     // public static int charMatches(String A, String B) {
@@ -100,17 +100,17 @@ public class SpellCheck{
     //     }
     //     for (int i = 1; i < first.length()-1; i++) {//runs through shorter word and checks whether the other word has a matching character within one position
     //         if (first.charAt(i) == second.charAt(i) ||
-    // 		first.charAt(i) == second.charAt(i-1) ||
-    // 		 first.charAt(i) == second.charAt(i+1))
-    // 		{
+    //      first.charAt(i) == second.charAt(i-1) ||
+    //       first.charAt(i) == second.charAt(i+1))
+    //      {
     //             //System.out.println(first.charAt(i));
     //             //System.out.println(second.charAt(i));
     //             matchCount += 1;
     //         }
     //     }
-    // 	if (first.length() == second.length()) { //will increase matchRatio if words are same length
-    // 	    matchCount += 1;
-    // 	}
+    //  if (first.length() == second.length()) { //will increase matchRatio if words are same length
+    //      matchCount += 1;
+    //  }
     //     return matchCount;
     // }
 
@@ -120,94 +120,91 @@ public class SpellCheck{
 
 
     private static ArrayList<String> listPotential(ArrayList<String> dict, String word) {
-	ArrayList<String> potential = new ArrayList<String>();
-	int low = 0;
-	int high = dict.size() - 1;
-	while (high >= low) {
-	    int mid = (low + high) / 2;
+        ArrayList<String> potential = new ArrayList<String>();
+        int low = 0;
+        int high = dict.size() - 1;
+        while (high >= low) {
+            int mid = (low + high) / 2;
             String testWord=dict.get(mid);
-	    if(testWord.compareTo(word) == 0) {
-		potential.clear();
-		potential.add(word);
-		return potential;
-	    }
-	    if(testWord.compareTo(word) < 0) {
-		low = mid + 1;
-		System.out.println(testWord + " was lower");
-		if (matchRatio(testWord, word) > 0.2) {
-		    for(int i = -150; i <= 150; i++) {//if there is a similar word it adds the entire region as potential match
-			if(!(dict.get(mid + i).length() - 3 >= word.length())){
-			potential.add(dict.get(mid + i));
-			}
-		    }
-		}
-		    
-		    
-	    }
-	    if(testWord.compareTo(word) > 0) {
-		high = mid - 1;
-		System.out.println(testWord+" was higher");
-		if (matchRatio(testWord, word) > 0.2) {
-		    for(int i = -150; i <= 150; i++) {
-			if(!(dict.get(mid + i).length() - 3 >= word.length())) {
-			potential.add(dict.get(mid + i));
-			}
-		    }
-		}
-	    }
-	    }
-	System.out.println("finished");
-	System.out.println(potential.toString());
-	return potential;
+            if (testWord.compareTo(word) == 0) {
+                potential.clear();
+                potential.add(word);
+                return potential;
+            }
+            if (testWord.compareTo(word) < 0) {
+                low = mid + 1;
+                System.out.println(testWord + " was lower");
+                if (matchRatio(testWord, word) > 0.2) {
+                    for(int i = -150; i <= 150; i++) {//if there is a similar word it adds the entire region as potential match
+                        if(!(dict.get(mid + i).length() - 3 >= word.length())){
+                            potential.add(dict.get(mid + i));
+                        }
+                    }
+                } 
+            }
+            if(testWord.compareTo(word) > 0) {
+                high = mid - 1;
+                System.out.println(testWord+" was higher");
+                if (matchRatio(testWord, word) > 0.2) {
+                        for(int i = -150; i <= 150; i++) {
+                            if(!(dict.get(mid + i).length() - 3 >= word.length())) {
+                                potential.add(dict.get(mid + i));
+                            }
+                        }
+                    }
+                }
+            }
+        System.out.println("finished");
+        System.out.println(potential.toString());
+        return potential;
     }
 
     public static String testPotential(String word) {
-	ArrayList<String> p = listPotential(alphabetical, word);
-	String out = "";
-	for (int i = 0; i < p.size(); i++) {
-	    out += p.get(i) + " ";
-	}
-	return out;
+        ArrayList<String> p = listPotential(alphabetical, word);
+        String out = "";
+        for (int i = 0; i < p.size(); i++) {
+            out += p.get(i) + " ";
+        }
+        return out;
     }
 
     public static ArrayList<String> reversePotential(ArrayList<String> potent) {
-	for (int i = 0; i < potent.size(); i++) {
-	    potent.set(i, new StringBuilder(potent.get(i)).reverse().toString());
-	}
-	return potent;
+        for (int i = 0; i < potent.size(); i++) {
+            potent.set(i, new StringBuilder(potent.get(i)).reverse().toString());
+        }
+        return potent;
     }
-	    
+        
 
-	
-     public static String bestMatcher(String word) {
-	 String bestMatch = "";
-	 ArrayList<String> toBeSearch = listPotential(alphabetical, word);
-	 toBeSearch.addAll(reversePotential(listPotential(reversed, new StringBuilder(word).reverse().toString())));
-	 for (int i = 0; i < toBeSearch.size(); i++) {
-	     //System.out.println(toBeSearch.size());
-	     if (matchRatio(word, toBeSearch.get(i)) > matchRatio(word, bestMatch)) {
-		 bestMatch = toBeSearch.get(i);
-	     }
-	 }
-	 return bestMatch;
-     }
+    
+    public static String bestMatcher(String word) {
+        String bestMatch = "";
+        ArrayList<String> toBeSearch = listPotential(alphabetical, word);
+        toBeSearch.addAll(reversePotential(listPotential(reversed, new StringBuilder(word).reverse().toString())));
+        for (int i = 0; i < toBeSearch.size(); i++) {
+            //System.out.println(toBeSearch.size());
+            if (matchRatio(word, toBeSearch.get(i)) > matchRatio(word, bestMatch)) {
+                bestMatch = toBeSearch.get(i);
+            }
+        }
+        return bestMatch;
+    }
 
-     public static String checkWords(String input){
-	String output = "";
-	dictionaryToArray();
-	commoners();
-	String[] inputText = input.replaceAll("\\p{P}", " ").toLowerCase().split(" ");
+    public static String checkWords(String input){
+        String output = "";
+        dictionaryToArray();
+        commoners();
+        String[] inputText = input.replaceAll("\\p{P}", " ").toLowerCase().split(" ");
         changed = new ArrayList<String[]>();
-	for(int i = 0; i < inputText.length; i++) {
-	    if (alphabetical.contains(inputText[i])){
-		output += inputText[i] + " ";
-	    }
-	    else {
-		output += bestMatcher(inputText[i]) + " ";
-	    }
-	}
-     
-	return output;
+        for(int i = 0; i < inputText.length; i++) {
+            if (alphabetical.contains(inputText[i])){
+                output += inputText[i] + " ";
+            }
+            else {
+                output += bestMatcher(inputText[i]) + " ";
+            }
+        }
+        return output;
     }
   
 
@@ -215,16 +212,16 @@ public class SpellCheck{
     
     public static void main(String[] args){
         Window w = new Window();
-	 w.setVisible(true);
-	 //System.out.println(matchRatio("quadratic","chicken"));
-	 //System.out.println(matchRatio("pisza", "pizza"));
-	 System.out.println(matchRatio("helo", "hello"));
-	 System.out.println(charMatches("watermelon", "watermolon"));
-	 System.out.println(charMatches("watermolon", "watercolor"));
+        w.setVisible(true);
+        //System.out.println(matchRatio("quadratic","chicken"));
+        //System.out.println(matchRatio("pisza", "pizza"));
+        System.out.println(matchRatio("helo", "hello"));
+        System.out.println(charMatches("watermelon", "watermolon"));
+        System.out.println(charMatches("watermolon", "watercolor"));
         dictionaryToArray();
-	//System.out.println(SpellCheck.testPotential("chicken"));
-	//System.out.println(alphabetical);
-	//System.out.println(reversed);
+        //System.out.println(SpellCheck.testPotential("chicken"));
+        //System.out.println(alphabetical);
+        //System.out.println(reversed);
     }
 
 }
